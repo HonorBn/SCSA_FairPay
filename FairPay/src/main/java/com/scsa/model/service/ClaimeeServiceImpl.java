474a9fddf27206dd3a3a99c755bc68dadb8f3ff3
youngbin-ahn <ahnbnn@gmail.com>
2017-12-15 16:07:20 +0900

    claim user account etc

--- FairPay/src/main/java/com/scsa/model/service/ClaimeeServiceImpl.java
+++ FairPay/src/main/java/com/scsa/model/service/ClaimeeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.scsa.model.service;
 
 import com.scsa.model.dao.ClaimeeDAO;
+import com.scsa.model.vo.ClaimInfo;
 import com.scsa.model.vo.ClaimeeInfo;
 
 public class ClaimeeServiceImpl implements ClaimeeService {
@@ -27,48 +28,8 @@ public class ClaimeeServiceImpl implements ClaimeeService {
 	}
 
 	@Override
-	public ClaimeeInfo selectClaimeeBefore(String paymentId) {
-		return claimeeDao.selectClaimeeBefore(paymentId);
+	public ClaimInfo getClaimeeByPaymentId(String paymentId) {
+		return claimeeDao.selectClaimeeByPaymentId(paymentId);
 	}
 	
-	@Override
-	public ClaimeeInfo selectClaimeeAfter(String paymentId) {
-		return claimeeDao.selectClaimeeAfter(paymentId);
-	}
-
-/*	@Override
-	public List<ClaimeeInfo> getClaimeeListByClaimerId(String claimerId) {
-		return claimeeDao.selectClaimeeListByClaimerId(claimerId);
-	}
-
-	@Override
-	public List<ClaimeeInfo> getClaimeeListByClaimeeId(String claimeeId) {
-		return claimeeDao.selectClaimeeListByClaimeeId(claimeeId);
-	}
-
-	@Override
-	public List<ClaimeeInfo> getClaimeeListById(String userId) {
-		return claimeeDao.selectClaimeeListById(userId);
-	}
-
-	@Override
-	public List<ClaimeeInfo> getPaidClaimeeList(String userId) {
-		return claimeeDao.selectPaidClaimeeList(userId);
-	}
-
-	@Override
-	public List<ClaimeeInfo> getUnpaidClaimeeList(String userId) {
-		return claimeeDao.selectUnpaidClaimeeList(userId);
-	}
-
-	@Override
-	public String getClaimByPaymentId(String paymentId) {
-		return claimeeDao.selectClaimByPaymentId(paymentId);
-	}
-
-	@Override
-	public ClaimeeInfo getClaimeeByPaymentId(String paymentId) {
-		return claimeeDao.selectClaimeeByPaymentId(paymentId);
-	} */
-
 }
--- FairPay/src/main/java/com/scsa/model/vo/ClaimeeInfo.java
+++ FairPay/src/main/java/com/scsa/model/vo/ClaimeeInfo.java
@@ -7,38 +7,34 @@ public class ClaimeeInfo {
 	private int isPaid;
 	private int tran_amt;
 	private String claimId;
-	private String claimeeId;
-	private String claimee_accountNumber;
-
-	private UserInfo claimeeUserInfo;
+	
+	UserInfo claimee;
+	AccountInfo account;
 	
 	public ClaimeeInfo() {
 		super();
 	}
 	
-	// 청구 등록할 때
-	public ClaimeeInfo(int tran_amt, String claimId, UserInfo claimeeUserInfo) {
+	// 청구 등록할 때	(paymentId, paymentDate : 자동 입력)
+	public ClaimeeInfo(int tran_amt, UserInfo claimee) {
 		super();
 		this.isPaid = 0;
 		this.tran_amt = tran_amt;
-		this.claimId = claimId;
-		this.claimeeId = claimeeUserInfo.getUserId();
-		this.claimeeUserInfo = claimeeUserInfo;
+		this.claimee = claimee;
 	}
 	
 	// 청구 수락했을 때
 	public ClaimeeInfo(String paymentId, String paymentDate, int isPaid, int tran_amt, String claimId,
-			String claimeeId, String claimee_accountNumber, UserInfo claimeeUserInfo) {
-		this(tran_amt, claimId, claimeeUserInfo);
-		this.claimeeId = claimeeId;
+			UserInfo claimee, AccountInfo account) {
+		this(tran_amt, claimee);
+		
 		this.paymentId = paymentId;
 		this.paymentDate = paymentDate;
 		this.isPaid = 0;
 		this.tran_amt = tran_amt;
 		this.claimId = claimId;
-		this.claimeeId = claimeeId;
-		this.claimee_accountNumber = claimee_accountNumber;
-		this.claimeeUserInfo = claimeeUserInfo;
+		this.claimee = claimee;
+		this.account = account;
 	}
 
 	public String getPaymentId() {
@@ -80,36 +76,28 @@ public class ClaimeeInfo {
 	public void setClaimId(String claimId) {
 		this.claimId = claimId;
 	}
-	
-	public String getClaimeeId() {
-		return claimeeId;
+
+	public UserInfo getClaimee() {
+		return claimee;
 	}
 
-	public void setClaimeeId(String claimeeId) {
-		this.claimeeId = claimeeId;
+	public void setClaimee(UserInfo claimee) {
+		this.claimee = claimee;
 	}
 
-	public String getClaimee_accountNumber() {
-		return claimee_accountNumber;
+	public AccountInfo getAccount() {
+		return account;
 	}
 
-	public void setClaimee_accountNumber(String claimee_accountNumber) {
-		this.claimee_accountNumber = claimee_accountNumber;
-	}
-
-	public UserInfo getClaimeeUserInfo() {
-		return claimeeUserInfo;
-	}
-
-	public void setClaimeeUserInfo(UserInfo claimeeUserInfo) {
-		this.claimeeUserInfo = claimeeUserInfo;
+	public void setAccount(AccountInfo account) {
+		this.account = account;
 	}
 
 	@Override
 	public String toString() {
 		return "ClaimeeInfo [paymentId=" + paymentId + ", paymentDate=" + paymentDate + ", isPaid=" + isPaid
-				+ ", tran_amt=" + tran_amt + ", claimId=" + claimId + ", claimeeId=" + claimeeId
-				+ ", claimee_accountNumber=" + claimee_accountNumber + ", claimeeUserInfo=" + claimeeUserInfo + "]";
+				+ ", tran_amt=" + tran_amt + ", claimId=" + claimId + ", claimee=" + claimee + ", account=" + account
+				+ "]";
 	}
 
 }
