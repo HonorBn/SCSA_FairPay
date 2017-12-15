17f07fdc035e2738fb4dc8f3afac4f1c43bbfc8b
youngbin-ahn <ahnbnn@gmail.com>
2017-12-15 21:16:50 +0900

    claim

--- FairPay/src/main/java/com/scsa/controller/ClaimController.java
+++ FairPay/src/main/java/com/scsa/controller/ClaimController.java
@@ -1,19 +1,23 @@
 package com.scsa.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import com.scsa.data.Ours;
 import com.scsa.data.Pay;
 import com.scsa.model.service.BankAPIService;
@@ -38,7 +42,7 @@ public class ClaimController {
 
 	// 청구 등록 후에 피청구와 영수증 등록
 	@RequestMapping(value = "/claim", method = RequestMethod.POST, produces = "text/plain;charset=utf-8")
-	public String addClaimInfo(@RequestBody ClaimInfo claim) throws IOException {
+	public String addClaimInfo(@RequestBody ClaimInfo claim) throws IOException, JSONException {
 		
 		// 청구 등록
 		claimService.createClaim(claim);
@@ -55,7 +59,7 @@ public class ClaimController {
 	}
 
 	// 피청구 등록, fcm push
-	private void addClaimeeInfo(ClaimInfo claim) throws IOException {
+	private void addClaimeeInfo(ClaimInfo claim) throws IOException, JSONException {
 		
 		List<ClaimeeInfo> claimeeList = claim.getClaimeeList();
 		
@@ -64,7 +68,7 @@ public class ClaimController {
 			claimeeService.createClaimee(claimee);
 		}
 		
-		//sendFCM(claimeeList);
+		sendFCM(claim);
 		
 	}
 
@@ -111,7 +115,13 @@ public class ClaimController {
 	
 	@RequestMapping(value = "/mine", method = RequestMethod.POST)
 	public List<ClaimInfo> selectClaimListDetailByMyId(@RequestBody String myId) {
-		return claimService.getClaimListDetailByMyId(myId); 
+		
+		System.out.println("내 아이디" + myId);
+		List<ClaimInfo> claimList = claimService.getClaimListDetailByMyId(myId);
+		
+		System.out.println(claimList.get(0).toString());
+		
+		return claimList;
 	}
 	
 	@RequestMapping(value = "/ours", method = RequestMethod.POST)
@@ -124,54 +134,88 @@ public class ClaimController {
 		return claimService.getClaimListDetailByOurId(ourId); 
 	}
 	
-	public void sendFCM(List<ClaimeeInfo> claimeeList) throws IOException {
+	
+	public void sendFCM(ClaimInfo claim) throws IOException, JSONException {
 		
-		String claimId = claimeeList.get(0).getClaimId();
+		List<ClaimeeInfo> claimeeList = claim.getClaimeeList();
+		
+		// prepare data
+		String claimId = claim.getClaimId();
+		String claimer = claim.getClaimer().getUsername();
 		int tokenSize = claimeeList.size();
 		String[] tokenList = new String[tokenSize];
-		for (int i = 0; i < tokenSize; i++) {
+		for (int i = 0; i < tokenSize; i++) 
 			tokenList[i] = claimeeList.get(i).getClaimee().getFcmId();
-		}
 		
-		String url = "https://android.googleapis.com/gcm/notification";
 		String contentType = "application/json";
 		String authorization = "key=AAAAhnbpQzw:APA91bEH0T4G9JF1vgMS2vQw695tQ7J_KwZHE6aUyvjQNCcs_fLn2Z2Xp9cMsnrmApIbRhHeSncuuLjuRgTVuI4gb8x5-sV11IdGPSvveDmwSZlGpgp-omI6_fiDpGhbWeu1zEQuy9H_";
 		String projectId = "577520616252";
 		
-		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("Authorization", authorization);
-		map.put("Content-Type", contentType);
-		map.put("project_id", projectId);
-		headers.setAll(map);
-		System.out.println(tokenSize);
-		Map req_payload = new HashMap();
-		req_payload.put("operation", "create");
-		req_payload.put("notification_key_name", claimId);
-		req_payload.put("registration_ids", tokenList);
+		// HTTP URL
+		URL url = new URL("https://android.googleapis.com/gcm/notification");
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setDoOutput(true);
 		
-		HttpEntity<Map> request = new HttpEntity<Map>(req_payload, headers);
+	    // HTTP request header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("Content-Type", contentType);
+	    con.setRequestProperty("Authorization", authorization);
+	    con.setRequestProperty("project_id", projectId);
+	    con.setDoOutput(true);
+	    con.connect();
+	    
+	    // HTTP request
+	    JSONObject payload = new JSONObject();
+	    payload.put("operation", "create");
+	    payload.put("notification_key_name", claimId);
+	    payload.put("registration_ids", new JSONArray(tokenList));
+	    
+	    OutputStream os = con.getOutputStream();
+	    os.write(payload.toString().getBytes("UTF-8"));
+	    os.flush();
+	    os.close();
+	    
+        // Read the response into a string
+        InputStream is = con.getInputStream();
+        String responseString = new Scanner(is, "UTF-8").useDelimiter("\\A").next();
+        is.close();
+
+        // Parse the JSON string and return the notification key
+        String notificationKey = new JSONObject(responseString).getString("notification_key");
+        
+		// HTTP URL
+		url = new URL("https://fcm.googleapis.com/fcm/send");
+		con = (HttpURLConnection) url.openConnection();
+		con.setDoOutput(true);
 		
-		String notificationKey = (String) (new RestTemplate().postForObject(url, request, Map.class).get("notification_key"));
-		
-		
-		url = "https://fcm.googleapis.com/fcm/send";
-		
-		headers = new LinkedMultiValueMap<String, String>();
-		map = new HashMap<String, String>();
-		map.put("Authorization", authorization);
-		map.put("Content-Type", contentType);
-		headers.setAll(map);
-		
-		Map data = new HashMap();
-		data.put("message", "신규 청구가 있습니다");
-		
-		req_payload = new HashMap();
-		req_payload.put("to", notificationKey);
-		req_payload.put("data", data);
-		
-		request = new HttpEntity<Map>(req_payload, headers);
-		
-		System.out.println((new RestTemplate().postForObject(url, request, Map.class).get("success")));
+	    // HTTP request header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("Content-Type", contentType);
+	    con.setRequestProperty("Authorization", authorization);
+	    con.setDoOutput(true);
+	    con.connect();
+	    
+	    // HTTP request
+	    JSONObject data = new JSONObject();
+	    data.put("message", claimer + "님이 새로운 청구를 요청했습니다");
+	    payload = new JSONObject();
+	    payload.put("to", notificationKey);
+	    payload.put("data", data);
+	    
+	    os = con.getOutputStream();
+	    os.write(payload.toString().getBytes("UTF-8"));
+	    os.flush();
+	    os.close();
+	    
+	    // Read the response into a string
+        is = con.getInputStream();
+        responseString = new Scanner(is, "UTF-8").useDelimiter("\\A").next();
+        is.close();
+
+        // Parse the JSON string and return the notification key
+        String successNum = new JSONObject(responseString).getString("success");
+        
+        System.out.println(successNum);
 	}
+	
 }
